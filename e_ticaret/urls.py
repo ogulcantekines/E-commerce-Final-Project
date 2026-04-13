d6c34c2830a705bd8018861db7613b74296200fb
ogulcantekines <ogulcan.tekines@gmail.com>
2026-04-14 00:55:51 +0300

    fix: remove fake price, fix collection link, add custom 404 page

--- e_ticaret/urls.py
+++ e_ticaret/urls.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import render
+
+def custom_404(request, exception):
+    return render(request, '404.html', status=404)
+
+handler404 = custom_404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
